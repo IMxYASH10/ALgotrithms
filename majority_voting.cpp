263a652eef77ba2b74204480afeb88e7a9882a9c
Yash Patil <yashvardhanpatil10@gmail.com>
2024-12-23 12:09:47 +0530

    Add files via upload

---
+++ majority_voting.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+int find_majority (vector<int>nums, int n){
+    int votes = 0, candidate;
+    for (int i = 0; i < n; i++){
+        if (votes == 0){
+            votes = 1; 
+            candidate = nums[i];
+        }
+        else if(candidate == nums[i]){
+            votes++;
+        }
+        else votes--;
+    }
+    return candidate;
+};
+int main ()
+{   
+    vector<int> nums = {2,3,4,4,5,4,2,4,5,4};
+    int n = nums.size();
+    int majority1 = find_majority(nums, n);
+    cout << "the Mojority element is :" <<  majority1 << endl;
+    return 0;
+}
